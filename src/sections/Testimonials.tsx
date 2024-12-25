dda3f38e9515d93366f4c642208ee70e6a848141
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-25 13:02:18 +0200

    refactor: enhance Testimonials section layout and styling for better presentation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,21 +50,29 @@ export const TestimonialsSection = () => {
           title="What Clients about Me"
           description="Don't just take my word for it. See what my clients have to say about my work."
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <div
-                className="absolute inset-0 opacity-5 -z-10"
-                style={{
-                  backgroundImage: `url(${grainImage.src})`,
-                }}
-              ></div>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-8">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
