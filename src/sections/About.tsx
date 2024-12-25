4135ebb96787177c8139cb13bcf71ba5c9ca3a44
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-25 15:32:13 +0200

    style: enhance item presentation in About section with improved spacing and outline

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -91,7 +91,10 @@ export const AboutSection = () => {
             />
             <div>
               {toolboxItems.map((item) => (
-                <div key={item.title} className="inline-flex items-center">
+                <div
+                  key={item.title}
+                  className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+                >
                   <TechIcon component={item.iconType} />
                   <span className="font-semibold">{item.title}</span>
                 </div>
