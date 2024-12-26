2bdec8e7ae4787aab076953d545123998d66d9bc
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-26 15:39:55 +0200

    make project cards sticky

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,16 +1,18 @@
-import React, { PropsWithChildren } from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 import grainImage from "@/assets/images/grain.jpg";
 import { twMerge } from "tailwind-merge";
 export const Card = ({
   className,
   children,
-}: PropsWithChildren<{ className?: string }>) => {
+  ...props
+}: ComponentPropsWithoutRef<"div">) => {
   return (
     <div
       className={twMerge(
         "bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
       )}
+      {...props}
     >
       <div
         className="absolute inset-0 -z-10 opacity-5"
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,10 +57,13 @@ export const ProjectsSection = () => {
           description="See how I transformed concepts into engaging digital experiences."
         />
         <div className="flex flex-col mt-10 gap-20 md:mt-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, index) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none sticky"
+              style={{
+                top: `calc(64px + ${index * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
