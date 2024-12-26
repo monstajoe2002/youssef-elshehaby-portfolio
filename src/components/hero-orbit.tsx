7daf3f9d748bec18a42bf3c8933818ebbf95902c
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-26 14:53:35 +0200

    animate stars and sparkles

--- src/components/hero-orbit.tsx
+++ src/components/hero-orbit.tsx
@@ -6,22 +6,24 @@ export const HeroOrbit = ({
   rotation,
 }: PropsWithChildren<{ size: number; rotation: number }>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div
-        className="flex items-start justify-start"
-        style={{
-          width: `${size}px`,
-          height: `${size}px`,
-          transform: `rotate(${rotation}deg)`,
-        }}
-      >
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
+      <div className="animate-spin [animation-duration:10s] -z-20">
         <div
-          className="inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            width: `${size}px`,
+            height: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
           }}
         >
-          {children}
+          <div
+            className="inline-flex"
+            style={{
+              transform: `rotate(${rotation * -1}deg)`,
+            }}
+          >
+            {children}
+          </div>
         </div>
       </div>
     </div>
