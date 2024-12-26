f949897c10a8cb2d05e1f5277c9eeebcb0ce98e6
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-26 14:20:11 +0200

    add footer section with links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,45 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+const footerLinks = [
+  {
+    title: "YouTube",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    href: "#",
+  },
+  {
+    title: "LinkedIn",
+    href: "#",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+          <div className="text-white/40">
+            &copy; {new Date().getFullYear()}. All rights reserved.
+          </div>
+          <nav className="flex flex-col items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
