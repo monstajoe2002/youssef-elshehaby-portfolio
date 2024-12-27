0c38b86444144516905f369233e6c213e149e38e
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-27 13:21:56 +0200

    update location and remove book card

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,7 +10,7 @@ import ReactIcon from "@/assets/icons/react.svg";
 import TypescriptIcon from "@/assets/icons/typescript.svg";
 import ArcIcon from "@/assets/icons/arc-browser.svg";
 import GithubIcon from "@/assets/icons/github.svg";
-import mapImage from "@/assets/images/map.png";
+import mapImage from "@/assets/images/map.jpg";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import { CardHeader } from "@/components/card-header";
 import { ToolboxItems } from "@/components/toolbox-items";
@@ -93,7 +93,7 @@ export const AboutSection = () => {
         />
         <div className="mt-20 flex flex-col gap-8">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
-            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+            {/* <Card className="h-[320px] md:col-span-2 lg:col-span-1">
               <CardHeader
                 title="My Reads"
                 description="Explore the books shaping my perspectives."
@@ -101,8 +101,8 @@ export const AboutSection = () => {
               <div className="w-40 mx-auto mt-2 md:mt-0">
                 <Image src={bookImage} alt="Book cover" />
               </div>
-            </Card>
-            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+            </Card> */}
+            <Card className="h-[320px] md:col-span-full">
               <CardHeader
                 title="My Toolbox"
                 description="Explore the technologies and tools I use to craft exceptional
@@ -150,7 +150,7 @@ export const AboutSection = () => {
               <Image
                 src={mapImage}
                 alt="map"
-                className="h-full w-full object-cover object-left-top"
+                className="h-full w-full object-cover"
               />
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-sky-200 to-blue-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after: -outline-offset-2 after:rounded-full after:outline-gray-950/30">
                 <div className="absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-blue-400 -z-20 animate-ping [animation-duration:2s]"></div>
