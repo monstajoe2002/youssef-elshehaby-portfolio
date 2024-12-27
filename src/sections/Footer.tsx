ae7dbb61317058c8b1d84d261718aab81a425323
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-27 15:48:59 +0200

    fix footer links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import Link from "next/link";
 const footerLinks = [
   {
     title: "YouTube",
@@ -16,7 +17,7 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-sky-300/30 [mask-image:radial-gradient(50%_50%_at_bottom,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
@@ -25,14 +26,15 @@ export const Footer = () => {
           </div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
-              <a
-                href="#"
+              <Link
+                href={link.href}
+                target="_blank"
                 key={link.title}
                 className="inline-flex items-center gap-1.5"
               >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
-              </a>
+              </Link>
             ))}
           </nav>
         </div>
